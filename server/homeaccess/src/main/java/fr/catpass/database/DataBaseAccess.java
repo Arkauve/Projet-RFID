349d68eefa7259258ce4471345a9af29a543c018
DEJOUX Jordan <jordan.dejoux@outlook.fr>
2017-02-21 12:22:54 +0100

    Ajout des classes pour communication base de données

---
+++ server/homeaccess/src/main/java/fr/catpass/database/DataBaseAccess.java
@@ -0,0 +1,28 @@
+package fr.catpass.database;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Map;
+/**
+ * Created by Jordan on 21/02/2017.
+ */
+public interface DataBaseAccess extends AutoCloseable
+{
+    String getDbDateFormat();
+
+    ResultSet query(String query) throws SQLException;
+
+
+    Map<String, String> findOneAsMap(String query) throws SQLException;
+
+    ArrayList<Map<String, String>> findAllAsMap(String query) throws SQLException;
+
+    ResultSet concurrentQuery(String query) throws SQLException;
+
+    boolean update(String query) throws SQLException;
+    void trustedInsertFromMap(Map<String, String> data) throws SQLException;
+
+    boolean callUpdateProcedure(String query) throws SQLException;
+
+    Map<String, String> callSelectProcedureAsMap(String query) throws SQLException;
+}

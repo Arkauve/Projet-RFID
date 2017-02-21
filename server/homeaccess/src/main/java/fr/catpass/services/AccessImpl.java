565fffce84a5f6e7b80030810aa840c1cd305d09
DEJOUX Jordan <jordan.dejoux@outlook.fr>
2017-02-21 12:23:59 +0100

    Service de récupération de l'autorisation de l'animal

--- server/homeaccess/src/main/java/fr/catpass/services/AccessImpl.java
+++ server/homeaccess/src/main/java/fr/catpass/services/AccessImpl.java
@@ -1,6 +1,10 @@
 package fr.catpass.services;
 
+import fr.catpass.providers.AnimalProviders;
+
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Jordan on 07/02/2017.
@@ -8,4 +12,15 @@ import javax.ws.rs.Path;
 @Path("/homeacess")
 public class AccessImpl implements AccessService {
 
+    @Override
+    public Response getAutorisation(String GUID) {
+        try {
+            if(AnimalProviders.autorisation(GUID))
+                return Response.status(Response.Status.ACCEPTED).build();
+            else
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
 }
--- server/homeaccess/src/main/java/fr/catpass/services/AccessService.java
+++ server/homeaccess/src/main/java/fr/catpass/services/AccessService.java
@@ -1,7 +1,17 @@
 package fr.catpass.services;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by Jordan on 07/02/2017.
  */
 public interface AccessService {
+
+    @GET
+    @Path("/{GUID}")
+    Response getAutorisation(@PathParam("GUID") String GUID);
 }
